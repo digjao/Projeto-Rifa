936276e8e3f9c218e0ff90bf49295fb7026d0a51
Joao Pedro <oliveiraneto.joaopedro@gmail.com>
2024-05-14 10:45:35 -0400

    Ajustando alguns detalhes

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,22 +2,26 @@
 
 const Login = () => {
   return (
-      <div className="flex justify-center items-center h-screen ">
-        <div className="w-96 p-6  rounded-3xl  bg-[#EFEFEF]">
+    <div className="">
+      <header className="h-14 bg-[#B5B5B5] border-b-2 border-black flex items-center pl-4">
+        <h1>Logo</h1>
+      </header>
+      <div className="flex justify-center items-center mt-48">
+        <div className="w-96 p-6  rounded-3xl drop-shadow-lg border bg-[#EFEFEF]">
           <h1 className="mb-10 text-3xl block text-center font-semibold">Login</h1>
           <div>
-            <input type="text" id="username" className="border-b-2 border-black w-full text-xl px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600 mb-5 bg-[#EFEFEF]" placeholder="Email ou nome de usuário"></input>
+            <input type="text" id="username" className=" border-b-2 border-black w-full text-xl px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600 mb-5 bg-[#EFEFEF]" placeholder="Email ou nome de usuário"></input>
           </div>
           <div>
-            <input type="text" id="username" className="border-b-2 border-black w-full text-xl px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600 mb-5 bg-[#EFEFEF]" placeholder="Senha"></input>
+            <input type="text" id="username" className="border-b-2 border-black w-full text-xl px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600 mb-5 bg-[#EFEFEF] " placeholder="Senha"></input>
           </div>
           <div className="mt-3 flex justify-between items-center mb-5">
             <div>
               <input className="mr-1" type="checkbox"/>
-              <label htmlFor="">Lembrar de mim</label>
+              <label htmlFor="" className="font-semibold">Lembrar de mim</label>
             </div>
             <div>
-              <a href="#">Esqueceu a senha?</a>
+              <a href="#" className="font-semibold">Esqueceu a senha?</a>
             </div>
           </div>
           <div>
@@ -32,11 +36,12 @@ const Login = () => {
             <button type="submit" className="border-2 bg-indigo-700 text-white rounded-md py-1 w-48 text-xl border-black">Login</button>
           </div>
           <div className="mt-3 flex justify-center">
-            <h1>Não tem uma conta? <link rel="stylesheet" href="" />Inscreva-se</h1>
+            <h1 className="font-semibold">Não tem uma conta? </h1>
+            <a href="#" className="text-indigo-900 ml-2 underline">inscreva-se</a>
           </div>
         </div>
       </div>
-
+    </div>
   )
 }
 
