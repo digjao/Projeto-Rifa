1cc8c8dc54e73d7cb75e3e3f0cf425b31f53d9cd
Joao Pedro <oliveiraneto.joaopedro@gmail.com>
2024-05-15 14:03:15 -0400

    Alinhando o checkbox e corrigindo h1's

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -16,17 +16,17 @@ const Login = () => {
             <input type="passwod" id="password" className="border-b-2 border-black w-full text-xl px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600 mb-5 bg-[#EFEFEF] " placeholder="Senha"></input>
           </div>
           <div className="mt-3 flex justify-between items-center mb-5">
-            <div>
-              <input className="mr-1" type="checkbox"/>
-              <label htmlFor="" className="font-semibold">Lembrar de mim</label>
+            <div className="flex items-center">
+              <input className="h-4 w-4" type="checkbox"/>
+              <label htmlFor="" className="font-semibold ml-2">Lembrar de mim</label>
             </div>
             <div>
-              <a href="#" className="font-semibold">Esqueceu a senha?</a>
+              <a href="#" className="font-bold">Esqueceu a senha?</a>
             </div>
           </div>
           <div>
               <div>
-                <h1 className="mb-4">Logar com google</h1>
+                <h1 className="mb-4">Logar com Google</h1>
               </div>
               <div>
                 <h1>Logar com Facebook</h1>
@@ -36,7 +36,7 @@ const Login = () => {
             <button type="submit" className="border-2 bg-[#7B66FD] text-white rounded-md py-1 w-48 text-xl border-black">Entrar</button>
           </div>
           <div className="mt-3 flex justify-center">
-            <h1 className="font-semibold">Não tem uma conta? </h1>
+            <h1 className="font-bold">Não tem uma conta? </h1>
             <a href="#" className="text-[#7B66FD] ml-2">Inscreva-se</a>
           </div>
         </div>
