deafe3af71812356c779ae2e6372c5b7d369157f
Joao Pedro <oliveiraneto.joaopedro@gmail.com>
2024-05-16 16:23:40 -0400

    Criando pagina de registro

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Login from "./components/Login"
 import ErrorPage from "./components/ErrorPage"
+import RegistrationForm from "./components/RegistrationForm"
+import LandingPage from "./components/LandingPage"
 
 
 
@@ -9,6 +11,8 @@ export const AppRoutes = () => {
     <BrowserRouter>
         <Routes>
             <Route path='/login' element={<Login />}/>
+            <Route path='/registro' element={<RegistrationForm />}/>
+            <Route path='/home' element={<LandingPage />}/>
 
 
             <Route path='*' element= {<Navigate to='/login' />}/>
