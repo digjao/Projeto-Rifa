bf09332e99235f5d5de81b87c8dca8b5d01ad7ca
Joao Pedro <oliveiraneto.joaopedro@gmail.com>
2024-06-06 16:59:42 -0400

    corrigindo o mesmo erro de novo

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Login from "./components/Login"
 import ErrorPage from "./components/ErrorPage"
 import RegistrationForm from "./components/RegistrationForm"
-import LandingPage from "./components/LandingPage"
+
 
 
 
