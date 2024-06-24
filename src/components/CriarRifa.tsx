45777db0f4fb1ff994c331c5b875f179ea517080
Joao Pedro <oliveiraneto.joaopedro@gmail.com>
2024-06-24 08:46:09 -0400

    Corrigindo problemas na branch

--- src/components/CriarRifa.tsx
+++ src/components/CriarRifa.tsx
@@ -5,43 +5,45 @@ export const CriarRifa = () => {
         <div className="flex flex-col min-h-screen">
             <NavBar />
             <h1 className="text-4xl p-4">Minha conta | Crie agora sua rifa</h1>
-            <div className="flex p-8">
-                <div className="p-2 grow">
+            <div className="flex flex-wrap p-8">
+                <div className="p-2 grow w-full md:w-1/2">
                     <form action="" className="flex flex-col">
-                        <h1 className="text-3xl p-2">Título da Rifa</h1>
-                        <input type="text" className="border-2 border-black p-2 rounded-3xl" placeholder="Ex: Rifa para ajudar..." />
-                        <h1 className="text-3xl p-2">Informe o objetivo da sua rifa</h1>
-                        <textarea className="border-2 border-black p-2 rounded-3xl h-32" />
-                        <div className="flex justify-between">
-                            <div>
-                                <h1 className="text-2xl p-2">Premio</h1>
-                                <input type="text" className="border-2 border-black p-2 rounded-3xl" placeholder="Ex: Tv 50'"/>
+                        <label htmlFor="tituloRifa" className="text-3xl p-2">Título da Rifa</label>
+                        <input type="text" id="tituloRifa" className="border-2 border-black p-2 rounded-3xl mb-4" placeholder="Ex: Rifa para ajudar..." />
+                        <label htmlFor="objetivoRifa" className="text-3xl p-2">Informe o objetivo da sua rifa</label>
+                        <textarea id="objetivoRifa" className="border-2 border-black p-2 rounded-3xl h-32 mb-4" />
+                        <div className="flex flex-wrap justify-between">
+                            <div className="w-full md:w-1/3 p-2">
+                                <label htmlFor="premioRifa" className="text-2xl p-2">Prêmio</label>
+                                <input type="text" id="premioRifa" className="border-2 border-black p-2 rounded-3xl w-full" placeholder="Ex: Tv 50'"/>
                             </div>
-                            <div>
-                                <h1 className="text-2xl p-2">Data do sorteio</h1>
-                                <input type="text" className="border-2 border-black p-2 rounded-3xl" placeholder="Ex: 99/99/9999"/>
+                            <div className="w-full md:w-1/3 p-2">
+                                <label htmlFor="dataSorteio" className="text-2xl p-2">Data do sorteio</label>
+                                <input type="date" id="dataSorteio" className="border-2 border-black p-2 rounded-3xl w-full" placeholder="Ex: 99/99/9999"/>
                             </div>
-                            <div>
-                                <h1 className="text-2xl p-2">Valor do bilhete</h1>
-                                <input type="text" className="border-2 border-black p-2 rounded-3xl" placeholder="Ex: R$0,00"/>
+                            <div className="w-full md:w-1/3 p-2">
+                                <label htmlFor="valorBilhete" className="text-2xl p-2">Valor do bilhete</label>
+                                <input type="number" step={0.01} id="valorBilhete" className="border-2 border-black p-2 rounded-3xl w-full" placeholder="Ex: R$0,00"/>
                             </div>
                         </div>
                     </form>
                 </div>
-                <div className="p-2 grow-0">
+                <div className="p-2 grow-0 w-full md:w-1/2">
                     <div className="flex flex-col">
-                        <h1 className="text-3xl p-2">Fotos do Prêmio</h1>
-                        <input type="file" className="border-2 border-black p-2 rounded-3xl h-80" />
+                        <label htmlFor="fotosPremio" className="text-3xl p-2">Fotos do Prêmio</label>
+                        <input type="file" id="fotosPremio" className="border-2 border-black p-2 rounded-3xl h-80" />
                     </div>
                 </div>
             </div>
             <div className="p-8">
-                <h1>Faça um upload da foto para confirmar o seu objetivo da rifa, isso transmite mais veracidade e consegue adquirir mais recursos para sua rifa.</h1>
-                <h1>Imagens com tamanho mínimo de 377x312</h1>
-                <h1>Imagens do tipo: jpg, jpeg ou png</h1>
+                <ul className="list-disc pl-5">
+                    <li>Faça um upload da foto para confirmar o seu objetivo da rifa, isso transmite mais veracidade e consegue adquirir mais recursos para sua rifa.</li>
+                    <li>Imagens com tamanho mínimo de 377x312</li>
+                    <li>Imagens do tipo: jpg, jpeg ou png</li>
+                </ul>
             </div>
             <div className="flex items-center justify-center p-8">
-            <button type="submit" className="w-64 mr-20 border-2 bg-[#7B66FD] text-white rounded-md py-1 text-xl border-black">Criar Rifa</button>
+                <button type="submit" className="w-64 mr-20 border-2 bg-[#7B66FD] text-white rounded-md py-1 text-xl border-black">Criar Rifa</button>
             </div>
         </div>
     )
