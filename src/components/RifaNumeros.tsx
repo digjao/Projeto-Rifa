27508a44e0e8684971177c02f145ea9a555e23c9
Joao Pedro <oliveiraneto.joaopedro@gmail.com>
2024-07-02 11:14:18 -0400

    Adicionando lógica de quantidade de números

--- src/components/RifaNumeros.tsx
+++ src/components/RifaNumeros.tsx
@@ -3,19 +3,19 @@ import ItemComponente from './ItemComponente';
 import NavBar from './NavBar';
 import { rifasInfo } from '../Interfaces/rifasInfo.ts';
 
-const rifas: rifasInfo[] = [
+const rifas: rifasInfo = 
   {
     rifa_id: 1,
     nome: 'Tv 50 polegadas',
     descricao: 'Belissima Tv',
     status: 'Disponível',
     preco_bilhete: 10,
-    quant_bilhetes: 100,
+    quant_bilhetes: 60,
     quant_comprados: 50,
     quant_restantes: 50,
     image_url: 'url_da_imagem_1',
   }
-];
+;
 
 const numeros = Array.from({ length: 80 }, (_, i) => i + 1);
 
@@ -25,18 +25,23 @@ export const RifaNumeros: React.FC = () => {
       <NavBar />
       <div className='flex p-20 justify-evenly'>
         <div className='bg-[#D9D9D9] rounded-lg border-2 border-black p-4 flex items-center justify-center'>
-          <div className='grid grid-cols-8 gap-2'>
+          <div className='grid grid-cols-8 gap-4'>
             {numeros.map((numero) => (
-              <button key={numero} className='w-12 h-12 border-2 border-black rounded-full hover:bg-gray-500'>
+              <button 
+              disabled={numero > rifas.quant_bilhetes} 
+                key={numero} 
+                className={`w-12 h-12 border-2 border-black rounded-full' 
+                ${numero > rifas.quant_bilhetes ? 'bg-gray-500 cursor-not-allowed' : 'hover:bg-gray-500 '}`} 
+                >
                 {numero}
               </button>
             ))}
           </div>
         </div>
         <div className='flex'>
-          {rifas.map((rifa) => (
-            <ItemComponente key={rifa.rifa_id} rifas={rifa} />
-          ))}
+         
+            <ItemComponente key={rifas.rifa_id} rifas={rifas} />
+         
         </div>
       </div>
     </div>
